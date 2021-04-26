b509a3b3fa027a961cba5a8ae33569019b607499
hassanshuhood <hassanshuhood@gmail.com>
2021-04-26 09:35:57 +0530

    pattern

---
+++ src/designPatterns/factorypattern/store/ChicagoPizzaStore.java
@@ -0,0 +1,20 @@
+package designPatterns.factorypattern.store;
+
+import designPatterns.factorypattern.pizza.*;
+
+public class ChicagoPizzaStore extends PizzaStore {
+
+        @Override
+        protected Pizza createPizza(String item) {
+            if (item.equals("cheese")) {
+                return new NYStyleCheesePizza();
+            } else if (item.equals("veggie")) {
+                return new ChicagoStyleCheesePizza();
+                // } else if (item.equals("clam")) {
+                // return new NYStyleClamPizza();
+                // } else if (item.equals("pepperoni")) {
+                // return new NYStylePepperoniPizza();
+            } else
+                return null;
+        }
+    }
---
+++ src/designPatterns/factorypattern/store/PizzaStore.java
@@ -0,0 +1,18 @@
+package designPatterns.factorypattern.store;
+
+import designPatterns.factorypattern.pizza.*;
+
+public abstract class PizzaStore {
+    public Pizza orderPizza(String type) {
+        Pizza pizza;
+        pizza = createPizza(type);
+        pizza.prepare();
+        pizza.bake();
+        pizza.cut();
+        pizza.box();
+        return pizza;
+    }
+
+    protected abstract Pizza createPizza(String type);
+    // other methods here
+}
